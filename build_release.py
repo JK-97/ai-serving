ece4b5895f328e7354c546c54e8dead1f7d66856
JK-97 <1179160244@qq.com>
2019-08-08 11:38:54 +0800

    Fix build script for cross platform

--- build_release.py
+++ build_release.py
@@ -1,4 +1,12 @@
 import os
+import sys, platform
+
+major_ver = "api_v1"
+minor_ver = "alpha"
+
+micro_arch = platform.machine()
+py_major = str(sys.version_info[0])
+py_minor = str(sys.version_info[1])
 
 # Cleaning
 print(">> Cleaning...")
@@ -8,9 +16,6 @@ if os.path.exists(os.path.join(os.getcwd(), 'release-pack')):
     os.system("rm -r release-pack")
 
 # Building
-major_ver = "api_v1"
-minor_ver = "alpha"
-
 print(">> Building", major_ver, "/", minor_ver, "...")
 source_files = [
     "serving/core/serving.py",
@@ -28,7 +33,6 @@ with open("srcs.txt", "w") as f:
 os.system("python3 setup.py build_ext")
 os.system("rm srcs.txt")
 
-
 # Constructing
 print(">> Constructing...")
 os.system("cp -r src release-pack")
@@ -36,10 +40,10 @@ for s in source_files:
     rm_cmd = "rm release-pack/" + s.replace(".py", ".*")
     os.system(rm_cmd)
     so_name = (s.split('/')[-1]).split('.')[0]
-    cp_cmd = "cp build/lib.linux-aarch64-3.6/" + so_name + ".cpython-36m-aarch64-linux-gnu.so release-pack/" + s.replace(".py", ".so")
+    cp_cmd = "cp build/lib.linux-"+micro_arch+"-"+py_major+"."+py_minor+"/"+so_name+".cpython-"+py_major+py_minor+"m-"+micro_arch+"-linux-gnu.so release-pack/"+s.replace(".py", ".so")
     os.system(cp_cmd)
-if os.path.exists(os.path.join(os.getcwd(), 'build')):
-    os.system("rm -r build")
+#if os.path.exists(os.path.join(os.getcwd(), 'build')):
+#    os.system("rm -r build")
 
 print("Done")
 
